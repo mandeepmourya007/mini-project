cffe410c9ab1161cdcc50f6f301a60b2985179c2
mandeep <mandeepmourya007@gmail.com>
2020-11-20 01:21:21 +0530

    gui updates

--- gui.py
+++ gui.py
@@ -1,42 +1,52 @@
 from tkinter import *
-import csv
-#main
-def run():
-    name1 = lecture.get()
-    print(name1)
-    # for i in :
-    a=text_box.get("1.0", END).split()
-    print(a)
-    rows = []
-    for i in a:
-        rows+=[[i]]
-    
-    with open("test.csv", 'w') as csvfile: 
-    
-        csvwriter = csv.writer(csvfile) 
-        csvwriter.writerow([name1]) 
-    
-        csvwriter.writerows(rows)
-  
+
 screen = Tk()
-screen.title("Attendance")
-screen.geometry("500x500")
-
-welcome_text = Label(text = "Welcome to our Attendance program ", fg = "red", bg = "yellow")
-welcome_text.pack()
-
-click_me = Button(text = "Click me", fg = "red", bg = "yellow", command = run)
-click_me.place(x = 10, y =20)
-
-lecture = StringVar()
-lecture_name = Entry(textvariable = lecture)
-lecture_name.pack()
-text_box = Text()
-text_box.pack()
-
-# T = Text(screen, height = 20, width = 25) 
+screen.title("Attendace") 
+subject = StringVar(screen)
+roll = StringVar(screen)
 
 
-# T.insert(END, "") 
-# T.pack() 
-screen.mainloop()
+def save():
+    s = subject.get()
+    studts = roll.get("1.0", "end")
+    print(s, studts)
+
+
+if __name__ == "__main__":
+
+    # Set the background colour of GUI window
+    screen.configure(background="light green")
+
+    # Set the configuration of GUI window (WidthxHeight)
+    screen.geometry("550x550")
+
+    # Create welcome to Real Time Currency Convertor label
+    headlabel = Label(
+        screen, text="welcome to Real Attendace visualizer", fg="black", bg="red"
+    )
+
+    label2 = Label(screen, text="Select subject", fg="black", bg="dark green")
+
+    headlabel.grid(row=0, column=1)
+    # label1.grid(row = 1, column = 0)
+    label2.grid(row=2, column=0)
+
+    # Create a text entry box
+    # for filling or typing the information.
+    roll = Text(screen, height=20, width=25, font="lucida 13")
+
+    roll.grid(row=5, column=1, ipadx="25")
+
+    # list of  subjects
+    subjects = ["RM", "ADA", "PPR", "MATHS"]
+
+    # create a drop down menu using OptionMenu function
+    options = OptionMenu(screen, subject, *subjects)
+
+    options.grid(row=2, column=1, ipadx=10)
+
+    submit = Button(screen, text="Submit", bg="red", fg="black", command=save)
+    submit.grid(row=6, column=1)
+
+    # Start the GUI
+    screen.mainloop()

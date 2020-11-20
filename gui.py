63fc9072fb865c961cc6562276d8cc74574bdd7e
mandeep <mandeepmourya007@gmail.com>
2020-11-20 13:39:31 +0530

    genrate file added

--- gui.py
+++ gui.py
@@ -1,15 +1,27 @@
 from tkinter import *
-
-screen = Tk()
-screen.title("Attendace")
-subject = StringVar(screen)
-roll = StringVar(screen)
-TOTAL_STUDENT=15
+import time
+import datetime
+TOTAL_STUDENT = 15
+# list of  subjects
+SUBJECTS = ["ADA", "PPR", "MATHS", "RM"]
 ADA = [0] * TOTAL_STUDENT
 MATHS = [0] * TOTAL_STUDENT
 PPR = [0] * TOTAL_STUDENT
 RM = [0] * TOTAL_STUDENT
 
+screen = Tk()
+screen.title("Attendace")
+subject = StringVar(screen)
+roll = StringVar(screen)
+
+def get_file_name():
+    x = datetime.datetime.now()
+    day=str(x.strftime("%x"))
+
+    day=day.split('/')
+    day="A" +day[1]+"_"+day[0]+"_"+day[2]
+    return day
+# print(get_file_name())
 
 def save():
     s = subject.get()
@@ -26,15 +38,14 @@ def start():
     for i in range(TOTAL_STUDENT):
         t = Checkbutton(
             screen,
-            text="MIT"+str(2020000+i),
+            text="MIT" + str(2020000 + i),
             variable=i,
             onvalue=1,
             offvalue=0,
-            command=lambda i=i: onClick(2020001+i),
+            command=lambda i=i: onClick(2020001 + i),
         )
         t.grid(row=10 + i, column=1, ipadx=10)
 
-        
 
 if __name__ == "__main__":
 
@@ -55,20 +66,11 @@ if __name__ == "__main__":
     # label1.grid(row = 1, column = 0)
     label2.grid(row=2, column=0)
     start()
-    # Create a text entry boxconfigure
-    # for filling or typing the information.
-    # roll = Text(screen, height=20, width=25, font="lucida 13")
-
-    # roll.grid(row=5, column=1, ipadx="25")
-    var1 = IntVar()
-    var2 = IntVar()
+    
 
     
-    # list of  subjects
-    subjects = ["RM", "ADA", "PPR", "MATHS"]
-
     # create a drop down menu using OptionMenu function
-    options = OptionMenu(screen, subject, *subjects)
+    options = OptionMenu(screen, subject, *SUBJECTS)
 
     options.grid(row=2, column=1, ipadx=10)
 

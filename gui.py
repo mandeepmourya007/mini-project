6477edb91c45b90f5b3d11e3a03154f6b13226af
mandeep <mandeepmourya007@gmail.com>
2020-11-20 12:57:57 +0530

    checkbox added

--- gui.py
+++ gui.py
@@ -1,9 +1,14 @@
 from tkinter import *
 
 screen = Tk()
-screen.title("Attendace") 
+screen.title("Attendace")
 subject = StringVar(screen)
 roll = StringVar(screen)
+TOTAL_STUDENT=15
+ADA = [0] * TOTAL_STUDENT
+MATHS = [0] * TOTAL_STUDENT
+PPR = [0] * TOTAL_STUDENT
+RM = [0] * TOTAL_STUDENT
 
 
 def save():
@@ -11,6 +16,25 @@ def save():
     studts = roll.get("1.0", "end")
     print(s, studts)
 
+
+def onClick(i):
+    print("present: MIT" + str(i))
+    return
+
+
+def start():
+    for i in range(TOTAL_STUDENT):
+        t = Checkbutton(
+            screen,
+            text="MIT"+str(2020000+i),
+            variable=i,
+            onvalue=1,
+            offvalue=0,
+            command=lambda i=i: onClick(2020001+i),
+        )
+        t.grid(row=10 + i, column=1, ipadx=10)
+
+        
 
 if __name__ == "__main__":
 
@@ -30,13 +54,16 @@ if __name__ == "__main__":
     headlabel.grid(row=0, column=1)
     # label1.grid(row = 1, column = 0)
     label2.grid(row=2, column=0)
-
-    # Create a text entry box
+    start()
+    # Create a text entry boxconfigure
     # for filling or typing the information.
-    roll = Text(screen, height=20, width=25, font="lucida 13")
+    # roll = Text(screen, height=20, width=25, font="lucida 13")
 
-    roll.grid(row=5, column=1, ipadx="25")
+    # roll.grid(row=5, column=1, ipadx="25")
+    var1 = IntVar()
+    var2 = IntVar()
 
+    
     # list of  subjects
     subjects = ["RM", "ADA", "PPR", "MATHS"]
 
